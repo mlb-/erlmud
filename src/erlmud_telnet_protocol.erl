b7a4460186a781e1c06b7255b09f400006af3b81
Matthew Batema <matthew.batema@gmail.com>
2012-08-01 12:31:58 -0700

    gen_server the protocol, for now.

--- src/erlmud_telnet_protocol.erl
+++ src/erlmud_telnet_protocol.erl
@@ -1,13 +1,63 @@
 -module(erlmud_telnet_protocol).
--export([start_link/4]).
--export([init/4]).
+-behaviour(gen_server).
 -include("log.hrl").
 
-start_link(ListenerPid, Socket, Transport, Opts) ->
-    Pid = spawn_link(?MODULE, init, [ListenerPid, Socket, Transport, Opts]),
-    {ok, Pid}.
+%%% Records
+-record(state, {
+        socket :: inet:socket(),
+        transport :: module(),
+        opts :: any()
+        }).
 
-init(ListenerPid, Socket, Transport, _Opts=[]) ->
+%%% Exports
+%% OTP API
+-export([start_link/4]).
+
+%% gen_server API
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
+
+%%% Functions
+%% OTP API
+start_link(ListenerPid, Socket, Transport, Opts) ->
+    State = #state{socket=Socket, transport=Transport, opts=Opts},
+    gen_server:start_link(?MODULE, {State, ListenerPid}, []).
+
+%% gen_server callbacks
+init({#state{}=State, ListenerPid}) ->
+    % Synchronous start_link/init, hence ranch accept delayed
+    gen_server:cast(self(), {post_init, ListenerPid}),
+    {ok, State}.
+
+handle_call(_Request, _From, State) ->
+    {reply, ok, State}.
+
+handle_cast({post_init, ListenerPid}, State) ->
+    post_init(ListenerPid, State),
+    {noreply, State};
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+handle_info(Info, State) ->
+    ?PRINT(Info),
+    once_active(State),
+    {noreply, State}.
+
+terminate(_Reason, #state{socket=Socket, transport=Transport}=_State) ->
+    Transport:close(Socket),
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%% internal
+post_init(ListenerPid, State) ->
     ok = ranch:accept_ack(ListenerPid),
-    Transport:send(Socket, <<"Hello world.\r\n">>),
-    Transport:close(Socket).
+    send(<<"Hello world.\r\n">>, State),
+    once_active(State).
+
+once_active(#state{socket=Socket, transport=Transport}) ->
+    Transport:setopts(Socket, [{active, once}]).
+
+send(Msg, #state{socket=Socket, transport=Transport}) ->
+    Transport:send(Socket, Msg).
