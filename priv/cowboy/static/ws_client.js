8106fa686a96f6c5c6ab9094c93d486562f338a7
Matthew Batema <matthew.batema@gmail.com>
2012-09-19 01:43:44 -0700

    Scroll websocket output
    Have a fixed-height div for WS output and a bottom element to insert
    before and then scroll to.

--- priv/cowboy/static/ws_client.js
+++ priv/cowboy/static/ws_client.js
@@ -2,19 +2,77 @@
 // support (via Modernizr).
 
 function ready(){
-  var ws = new WebSocket(document.location.toString().replace("http:", "ws:"));
+  var io = scaffold();
+  var input = io.input;
+  var output = io.output;
+  var ws = ws_setup(output);
+
+  input.bind('keydown', function(e){
+    if(e.keyCode == 13){
+      e.preventDefault();
+      var value = input.val();
+      input.val("");
+      if(value){
+        ws.send(value);
+        output(value);
+      }
+    }
+  });
+}
+
+function scaffold(){
+  // Cleanup unnecessary DOM elements
+  $('.support').remove();
+
+  // Scaffold new DOM
+  var frame = $('<div>');
+
+  var input = $('<input type="text">')
+    .attr({id: 'input'});
+  var output = $('<div>', {id: 'output'})
+    .css({
+      overflow: "scroll",
+      height: "300px"
+    });
+
+  var scrollTo = $('<div>', {id: 'scrollTo'});
+
+  output.append(scrollTo);
+
+  frame.append(output);
+  frame.append(input);
+
+  $('body').append(frame);
+
+  input.focus();
+  return {
+    input: input,
+      output: function(msg){
+        scrollTo.before(msg + "<br/>");
+        scrollTo[0].scrollIntoView();
+      }
+  };
+}
+
+function ws_setup(output){
+  var ws = new WebSocket(
+      document.location.toString().replace("http:", "ws:")
+      );
   ws.onopen = function() {
     // websocket is connected
-    $("#status").html("websocket connected!<br/>");
+    output("Welcome to erlmud!");
     // send hello data to server.
-    ws.send("hello server!");
+    ws.send("look");
   };
   ws.onmessage = function (evt) {
     var receivedMsg = evt.data;
-    $("#status").append(receivedMsg + "<br/>");
+    output(receivedMsg);
   };
   ws.onclose = function() {
     // websocket was closed
-    $("#status").append("websocket was closed");
+    output("websocket was closed");
   };
+  return ws;
 }
+
+// vim: fdm=indent
